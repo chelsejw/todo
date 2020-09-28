9fdeddc8476b924baf4467dc5503c832652519d8
chelsejw <chelsejw@gmail.com>
2020-09-28 19:56:47 +0800

    Updates

---
+++ precache-manifest.4c93c5f135f8b6107d3bde9e260914f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3b0ed077aa5c0dead84",
+    "url": "/hooks-todo/static/css/main.be9cd952.chunk.css"
+  },
+  {
+    "revision": "d3b0ed077aa5c0dead84",
+    "url": "/hooks-todo/static/js/main.d3b0ed07.chunk.js"
+  },
+  {
+    "revision": "60127afb8d8ba542115c",
+    "url": "/hooks-todo/static/js/runtime~main.60127afb.js"
+  },
+  {
+    "revision": "071d26344099d611d646",
+    "url": "/hooks-todo/static/css/2.eca465cf.chunk.css"
+  },
+  {
+    "revision": "071d26344099d611d646",
+    "url": "/hooks-todo/static/js/2.071d2634.chunk.js"
+  },
+  {
+    "revision": "33047bbb09470406f2f5d86d45206957",
+    "url": "/hooks-todo/index.html"
+  }
+];
