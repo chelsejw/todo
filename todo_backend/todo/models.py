940d1438b2b4145a934318ceb52d4e9770258a6f
chelsejw <chelsejw@gmail.com>
2020-09-28 20:37:25 +0800

    change status col to be completed, a boolean field

--- todo_backend/todo/models.py
+++ todo_backend/todo/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Item(models.Model):
     content = models.TextField()
-    status = models.CharField(max_length=50)
+    completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
